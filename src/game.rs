566b4c1bdada81f9cda67b8a6b234bd140a205e8
Samuel Skean <sskean2@uic.edu>
2025-03-21 13:57:22 -0500

    Sorta playable!
    Turns are on the honor system and we don't handle diagonals, but still!

--- src/game.rs
+++ src/game.rs
@@ -1,13 +1,34 @@
 use std::io;
 
-#[derive(Debug, Clone, Copy)]
+use thiserror::Error;
+
+const SIDE_LENGTH: u8 = 3;
+const BOARD_SIZE: u8 = SIDE_LENGTH * SIDE_LENGTH;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum GameSquare {
     X,
     O,
 }
 
-pub struct GameBoard([Option<GameSquare>; 3 * 3]);
+impl GameSquare {
+    pub fn name(self) -> &'static str {
+        match self {
+            GameSquare::X => "X",
+            GameSquare::O => "O",
+        }
+    }
+}
 
+pub struct GameBoard([Option<GameSquare>; BOARD_SIZE as usize]);
+
+#[derive(Debug, Error)]
+pub enum MoveError {
+    #[error("Index was too big. Pick a number from 1 through {BOARD_SIZE}.")]
+    PosTooBig(u8),
+    #[error("{} had already moved there.", .0.name())]
+    AlreadyMoved(GameSquare),
+}
 impl GameBoard {
     pub fn new() -> Self {
         GameBoard([None; 3 * 3])
@@ -16,16 +37,57 @@ impl GameBoard {
     pub fn encode_to(&self, sink: &mut impl io::Write) {
         for (i, square) in self.0.iter().enumerate() {
             let index_char = [(i + 1) as u8 + b'0'];
-            sink.write_all(match square {
-                Some(GameSquare::X) => b"X",
-                Some(GameSquare::O) => b"O",
-                None => &index_char,
-            })
-            .unwrap();
+            sink.write_all(square.map(|s| s.name().as_bytes()).unwrap_or(&index_char))
+                .unwrap();
             // Put newlines after every 3.
             if (i + 1) % 3 == 0 {
                 sink.write(b"\n").unwrap();
             }
         }
+    }
+
+    /// `pos` is 1-indexed.
+    pub fn make_move(&mut self, player: GameSquare, pos: u8) -> Result<bool, MoveError> {
+        let pos = pos - 1;
+        if pos >= BOARD_SIZE {
+            Err(MoveError::PosTooBig(pos))
+        } else if let Some(p) = self.0[pos as usize] {
+            Err(MoveError::AlreadyMoved(p))
+        } else {
+            self.0[pos as usize] = Some(player);
+            let winner = self.winner();
+            if winner != None && winner != Some(player) {
+                panic!("Made a move and the other guy won.");
+            }
+            Ok(winner == Some(player))
+        }
+    }
+
+    fn winner(&self) -> Option<GameSquare> {
+        fn check_pattern(
+            pattern: impl Iterator<Item = impl Iterator<Item = Option<GameSquare>>>,
+        ) -> Option<GameSquare> {
+            for mut series in pattern {
+                let first = series.next().unwrap();
+                if first == None {
+                    continue;
+                }
+                if series.all(|s| s == first) {
+                    return first;
+                }
+            }
+            None
+        }
+
+        let rows = self
+            .0
+            .chunks_exact(SIDE_LENGTH as usize)
+            .map(|s| s.iter().copied());
+        let columns =
+            (0..SIDE_LENGTH).map(|y| (0..SIDE_LENGTH).map(move |x| self.0[(3 * x + y) as usize]));
+
+        check_pattern(rows)
+            .or_else(|| check_pattern(columns))
+            .or_else(|| None /* TODO: Handle diagonals. */)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use game::*;
 const SERVER_SOCK_PATH: &str = "skean_tic_tac_toe.sock";
 const PROMPT: &[u8] = b"> ";
 
-fn prompt(game_board: &GameBoard, socket: &UnixDatagram, peer_addr: &SocketAddr, buf: &mut [u8]) {
-    let mut cursor = io::Cursor::new(buf);
+fn prompt(game_board: &GameBoard, socket: &UnixDatagram, peer_addr: &SocketAddr) {
+    let mut cursor = io::Cursor::new([0; 0x1000]);
     game_board.encode_to(&mut cursor);
     socket
         .send_to_addr(&cursor.get_ref()[..cursor.position() as usize], peer_addr)
@@ -43,20 +43,46 @@ fn main() {
         } {}
 
         dbg!(&first_peer_addr);
-        let game_board = GameBoard::new();
+        let mut game_board = GameBoard::new();
 
-        prompt(&game_board, &socket, &first_peer_addr, &mut buf);
-        prompt(&game_board, &socket, &second_peer_addr, &mut buf);
+        prompt(&game_board, &socket, &first_peer_addr);
+        prompt(&game_board, &socket, &second_peer_addr);
 
         loop {
-            let (_, peer_addr) = socket.recv_from(&mut buf).unwrap();
+            let (dgram_size, peer_addr) = socket.recv_from(&mut buf).unwrap();
 
-            if peer_addr.as_pathname() == first_peer_addr.as_pathname() {
-                prompt(&game_board, &socket, &first_peer_addr, &mut buf);
+            let pos: Result<u8, _> = std::str::from_utf8(&buf[..dgram_size - 1]).unwrap().parse();
+            let pos = match pos {
+                Ok(pos) => pos,
+                Err(e) => {
+                    socket.send_to_addr(e.to_string().as_bytes(), &peer_addr).unwrap();
+                    continue;
+                }
+            };
+
+            let player = if peer_addr.as_pathname() == first_peer_addr.as_pathname() {
+                GameSquare::X
             } else if peer_addr.as_pathname() == second_peer_addr.as_pathname() {
-                prompt(&game_board, &socket, &second_peer_addr, &mut buf);
+                GameSquare::O
             } else {
                 eprintln!("Unexpected packet from {peer_addr:?}");
+                continue;
+            };
+            match game_board.make_move(player, pos) {
+                Ok(true) => {
+                    // STRETCH: Might be nice to tell the player if *they* won.
+                    let win_msg = format!("{} won!\n", player.name());
+                    let _ = socket.send_to_addr(win_msg.as_bytes(), &first_peer_addr);
+                    let _ = socket.send_to_addr(win_msg.as_bytes(), &second_peer_addr);
+                    game_board = GameBoard::new();
+                    prompt(&game_board, &socket, &first_peer_addr);
+                    prompt(&game_board, &socket, &second_peer_addr);
+                }
+                Ok(false) => {
+                    prompt(&game_board, &socket, &peer_addr);
+                } Err(e) => {
+                    socket.send_to_addr(e.to_string().as_bytes(), &peer_addr).unwrap();
+                }
             }
         }
     }
