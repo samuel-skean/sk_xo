9a473aa1d3b0fd3ad15d6621d9ba642cd1490083
Samuel Skean <sskean2@uic.edu>
2025-03-21 22:10:56 -0500

    Implemented turns!

--- src/game.rs
+++ src/game.rs
@@ -6,33 +6,46 @@ const SIDE_LENGTH: u8 = 3;
 const BOARD_SIZE: u8 = SIDE_LENGTH * SIDE_LENGTH;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum GameSquare {
+pub enum Mark {
     X,
     O,
 }
 
-impl fmt::Display for GameSquare {
+impl Mark {
+    pub fn opponent(self) -> Self {
+        match self {
+            Mark::X => Mark::O,
+            Mark::O => Mark::X,
+        }
+    }
+}
+
+impl fmt::Display for Mark {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(match self {
-            GameSquare::X => "X",
-            GameSquare::O => "O",
+            Mark::X => "X",
+            Mark::O => "O",
         })
     }
 }
 
-pub struct GameBoard([Option<GameSquare>; BOARD_SIZE as usize]);
+pub struct Game {
+    board: [Option<Mark>; BOARD_SIZE as usize],
+    current_player: Mark,
+}
 
 #[derive(Debug, Error)]
 pub enum MoveError {
     #[error("Index was too big. Pick a number from 1 through {BOARD_SIZE}.")]
     PosTooBig(u8),
     #[error("{0} had already moved there.")]
-    AlreadyMoved(GameSquare),
+    AlreadyMoved(Mark),
 }
 
-impl fmt::Display for GameBoard {
+// TODO: Should GameBoard be a separate type? This doesn't exactly print the whole game...
+impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for (i, square) in self.0.iter().enumerate() {
+        for (i, square) in self.board.iter().enumerate() {
             if let Some(filled_square) = square {
                 write!(f, "{filled_square}")?;
             } else {
@@ -47,32 +60,41 @@ impl fmt::Display for GameBoard {
     }
 }
 
-impl GameBoard {
+impl Game {
     pub fn new() -> Self {
-        GameBoard([None; 3 * 3])
+        Self {
+            board: [None; 3 * 3],
+            current_player: Mark::X,
+        }
+    }
+
+    pub fn current_player(&self) -> Mark {
+        self.current_player
     }
 
     /// `pos` is 1-indexed.
-    pub fn make_move(&mut self, player: GameSquare, pos: u8) -> Result<bool, MoveError> {
+    pub fn make_move(&mut self, pos: u8) -> Result<bool, MoveError> {
+        let player = self.current_player;
         let pos = pos - 1;
         if pos >= BOARD_SIZE {
             Err(MoveError::PosTooBig(pos))
-        } else if let Some(p) = self.0[pos as usize] {
+        } else if let Some(p) = self.board[pos as usize] {
             Err(MoveError::AlreadyMoved(p))
         } else {
-            self.0[pos as usize] = Some(player);
+            self.board[pos as usize] = Some(player);
             let winner = self.winner();
             if winner != None && winner != Some(player) {
                 panic!("Made a move and the other guy won.");
             }
+            self.current_player = player.opponent();
             Ok(winner == Some(player))
         }
     }
 
-    fn winner(&self) -> Option<GameSquare> {
+    fn winner(&self) -> Option<Mark> {
         fn check_pattern(
-            pattern: impl Iterator<Item = impl Iterator<Item = Option<GameSquare>>>,
-        ) -> Option<GameSquare> {
+            pattern: impl Iterator<Item = impl Iterator<Item = Option<Mark>>>,
+        ) -> Option<Mark> {
             for mut series in pattern {
                 let first = series.next().unwrap();
                 if first == None {
@@ -86,11 +108,11 @@ impl GameBoard {
         }
 
         let rows = self
-            .0
+            .board
             .chunks_exact(SIDE_LENGTH as usize)
             .map(|s| s.iter().copied());
-        let columns =
-            (0..SIDE_LENGTH).map(|y| (0..SIDE_LENGTH).map(move |x| self.0[(3 * x + y) as usize]));
+        let columns = (0..SIDE_LENGTH)
+            .map(|y| (0..SIDE_LENGTH).map(move |x| self.board[(3 * x + y) as usize]));
 
         check_pattern(rows)
             .or_else(|| check_pattern(columns))
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,40 @@ mod game;
 use std::{
     fs,
     io::{self, Write},
+    ops::{Deref, DerefMut},
     os::unix::net::{SocketAddr, UnixDatagram},
 };
 
-use game::*;
+use game::Mark;
+
+struct NetworkedGame {
+    inner: game::Game,
+    x_addr: SocketAddr,
+    o_addr: SocketAddr,
+}
+
+impl NetworkedGame {
+    fn player_addr(&self, player: Mark) -> &SocketAddr {
+        match player {
+            Mark::X => &self.x_addr,
+            Mark::O => &self.o_addr,
+        }
+    }
+}
+
+impl Deref for NetworkedGame {
+    type Target = game::Game;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl DerefMut for NetworkedGame {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
 
 const SERVER_SOCK_PATH: &str = "sk_xo.sock";
 const PROMPT: &str = "> ";
@@ -16,9 +46,9 @@ const PROMPT: &str = "> ";
 // room, I think. How well optimized does this get? Does it check against other
 // failure conditions too?
 
-fn prompt(game_board: &GameBoard, socket: &UnixDatagram, peer_addr: &SocketAddr) {
+fn prompt(game: &NetworkedGame, socket: &UnixDatagram, peer_addr: &SocketAddr) {
     let mut cursor = io::Cursor::new([b'h'; 0x1000]);
-    write!(cursor, "{game_board}").unwrap();
+    write!(cursor, "{}", game.inner).unwrap();
     socket
         .send_to_addr(&cursor.get_ref()[..cursor.position() as usize], peer_addr)
         .unwrap();
@@ -49,13 +79,36 @@ fn main() {
         } {}
 
         dbg!(&first_peer_addr);
-        let mut game_board = GameBoard::new();
+        let mut game = NetworkedGame {
+            inner: game::Game::new(),
+            x_addr: first_peer_addr,
+            o_addr: second_peer_addr,
+        };
 
-        prompt(&game_board, &socket, &first_peer_addr);
-        prompt(&game_board, &socket, &second_peer_addr);
+        prompt(&game, &socket, &game.x_addr);
+        socket
+            .send_to_addr(b"The other player has joined.\n", &game.o_addr)
+            .unwrap();
 
         loop {
             let (dgram_size, peer_addr) = socket.recv_from(&mut scratch).unwrap();
+
+            let player = if peer_addr.as_pathname() == game.x_addr.as_pathname() {
+                Mark::X
+            } else if peer_addr.as_pathname() == game.o_addr.as_pathname() {
+                Mark::O
+            } else {
+                eprintln!("Unexpected packet from {peer_addr:?}");
+                continue;
+            };
+            // Should this be handled here or by an error in the game? I think
+            // it should be handled here, because other errors cause a new
+            // prompt. But I don't like how this interrupts the integer parsing
+            // code.
+            if game.current_player() != player {
+                socket.send_to_addr(b"It's not your turn.\n", &peer_addr).unwrap();
+                continue;
+            }
 
             let pos: Result<u8, _> = std::str::from_utf8(&scratch[..dgram_size - 1])
                 .unwrap()
@@ -77,29 +130,20 @@ fn main() {
                 }
             };
 
-            let player = if peer_addr.as_pathname() == first_peer_addr.as_pathname() {
-                GameSquare::X
-            } else if peer_addr.as_pathname() == second_peer_addr.as_pathname() {
-                GameSquare::O
-            } else {
-                eprintln!("Unexpected packet from {peer_addr:?}");
-                continue;
-            };
-            match game_board.make_move(player, pos) {
+            match game.make_move(pos) {
                 Ok(true) => {
                     // STRETCH: Might be nice to tell the player if *they* won.
                     // TODO: Always print this on *a* newline.
                     write!(scratch_cursor, "{player} won!\n").unwrap();
                     let written_to_scratch =
                         &scratch_cursor.get_ref()[..scratch_cursor.position() as usize];
-                    let _ = socket.send_to_addr(written_to_scratch, &first_peer_addr);
-                    let _ = socket.send_to_addr(written_to_scratch, &second_peer_addr);
-                    game_board = GameBoard::new();
-                    prompt(&game_board, &socket, &first_peer_addr);
-                    prompt(&game_board, &socket, &second_peer_addr);
+                    let _ = socket.send_to_addr(written_to_scratch, &game.x_addr);
+                    let _ = socket.send_to_addr(written_to_scratch, &game.o_addr);
+                    game.inner = game::Game::new();
+                    prompt(&game, &socket, &game.x_addr);
                 }
                 Ok(false) => {
-                    prompt(&game_board, &socket, &peer_addr);
+                    prompt(&game, &socket, game.player_addr(player.opponent()));
                 }
                 Err(e) => {
                     write!(scratch_cursor, "{e}\n{PROMPT}").unwrap();
